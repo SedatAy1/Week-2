aa776e8ae0cd23953ee8c1d9aa8fa88eadfc4ff9
Sedat Ay <sedatay30@gmail.com>
2024-03-08 09:34:03 +0300

    .

---
+++ src/MaasHesaplayicii.java
@@ -0,0 +1,11 @@
+public class MaasHesaplayicii {
+    public static void main(String[] args) {
+
+        MaasHesaplayici kemal = new MaasHesaplayici("Sedat", 2000, 45, 1995);
+        kemal.toSring();
+        MaasHesaplayici esra= new MaasHesaplayici("Ahmet",500,20,1970);
+        esra.toSring();
+
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    String adı;
+    String ders;
+    int sözlüNotu;
+    int yazılıNot;
+    double hesap;
+
+
+
+    Main(String adı, String ders, int sözlüNotu, int yazılıNot) {
+        this.adı = adı;
+        this.ders = ders;
+        this.sözlüNotu = sözlüNotu;
+        this.yazılıNot = yazılıNot;
+        this.hesap = hesap;
+    }
+    public void Hesapla() {
+        this.hesap = (this.yazılıNot * 0.20) + (this.yazılıNot * 0.80);
+        System.out.println("Öğrenci : " + this.adı);
+        System.out.println("Ders Adı : " + this.ders);
+        System.out.println("Hesaplanmış Puan : " + this.hesap);
+        System.out.println("----------------");
     }
 }
